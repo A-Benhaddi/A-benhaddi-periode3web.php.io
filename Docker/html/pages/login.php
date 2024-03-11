4e5a20d8825e45238aa40055293ff1ee5ab90f30
A-Benhaddi <abdelilahbenhaddi024@gmail.com>
2024-03-11 14:35:54 +0100

    login af

--- Docker/html/pages/login.php
+++ Docker/html/pages/login.php
@@ -2,31 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel = "stylesheet" type = "text/css" href = ".../ccs/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <form class="register_form" name="register" method="POST" action="register.php">
-        <h3>Register</h3>
-        <div class="row">
-            <p>Naam: </p>
-            <input type="text" name="naam"> 
-        </div>
-        <div class="row">
-            <p>Leeftijd: </p>
-            <input type="number" name="leeftijd"> 
-        </div>
-        <div class="row">
-            <p>Algemene voorwaarden: </p>
-            <input type="checkbox" name="av"> 
-        </div>
-        <div class="row">
-            <p>Geslacht: </p>
-            <input type="text" name="geslacht"> 
-        </div>
-        <div class="row">
-            <input type="submit" name="submit"> 
-        </div>
+    <form action='login.logic.php' name="login" method="POST">
+        <label> Username: </label>
+        <input type="text" name="username" placeholder="Username">
+        <label>Password:</label>
+        <input type ="password" name="password" placeholder="Password"> 
+        <input type="submit" value="Login">
     </form>
 </body>
 </html>
--- Docker/html/pages/register.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($_post'['leeftijd']<18){
-    header ("Location: ../index.php");
-}
-else{
-    echo'legal';
-}
-?>
