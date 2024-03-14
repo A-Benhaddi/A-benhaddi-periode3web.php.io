ac173ea5c31e4c0162fca9707526f214b0a8de7a
A-Benhaddi <abdelilahbenhaddi024@gmail.com>
2024-03-14 21:06:03 +0100

    home page af

--- Docker/html/index.php
+++ Docker/html/index.php
@@ -43,24 +43,37 @@
           </div>
         </div>
       </div>
-      <div class="boodschapen-container">
-        <div class="container-producten">
-          <div class="producten-linkeblok">
-            <div class="tekst-linkeblok"><h1>Producten</h1></div>
-            <img src="imgs/fruit.jpg">
-          </div>
-          <div class="producten-rechtenblok">
-            <div class="tekst-rechteblok">
-              <p>producten.</p>
+      <div class="slider-wrapper">
+              <div class="slider">
+                <img id="slide-1" src="https://www.datocms-assets.com/56706/1690435320-adobestock_479178344_boodschappen_supermarkt.jpg" alt="an image of a anime called one piece worth too give it a try">
+                <img id="slide-2" src="https://www.consultancy.nl/illustrations/news/spotlight/2023-01-10-090914566-dure_boodschappen.jpg" alt="an image about a game called risk of rain 2">
+                <img id="slide-3" src="https://blogo.nl/wp-content/uploads/2020/03/boodschappen-doen-met-je-peuter.jpg" alt="an bunch of images from the inside of big gym">
+              </div>
+              <div class="slider-nav">
+                <a href="#slide-1"></a>
+                <a href="#slide-2"></a>
+                <a href="#slide-3"></a>
+              </div>
             </div>
-            <img src="imgs/zuivel.jpg">
-          </div>
-        </div>
-      </div>
+            <div class="push6"></div>
     </main>
     <div class="openingstijden-container">
       <div class="tijden-container">
-        <div class="openingstijden"><h1>sdsdd</h1></div>
+        <div class="openingstijden"><h1>Openingstijden</h1></div>
+          <div class="openingstijden2">
+        <ul>
+            <li><strong>Maandag:</strong> 24/7</li>
+            <li><strong>Dinsdag:</strong> 24/7</li>
+            <li><strong>Woensdag:</strong> 24/7</li>
+            <li><strong>Donderdag:</strong> 24/7</li>
+            <li><strong>Vrijdag:</strong> 24/7</li>
+            <li><strong>Zaterdag:</strong> 24/7</li>
+            <li><strong>Zondag:</strong> 24/7</li>
+        </ul>
+      </div>
+      </div>
+      </div>
+
       </div>
     </div>
     <div class="push5"></div>
