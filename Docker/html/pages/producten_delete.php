bbcc11c1f86f25b6633af975e1199ad3bbd7b144
A-Benhaddi <abdelilahbenhaddi024@gmail.com>
2024-04-12 18:22:48 +0200

    done

--- Docker/html/pages/producten_delete.php
+++ Docker/html/pages/producten_delete.php
@@ -2,15 +2,22 @@
 session_start();
 include ("conn.php");
 
+if (!isset($_GET["id"]) || empty($_GET["id"])) {
+  die("Missing product ID");
+}
 
-$userid = $_POST["id"];
+$productid = $_GET["id"];
 
+// select a particular product by id
+$stmt = $connection->prepare("DELETE FROM Producten WHERE id=:productid"); 
+$stmt->bindParam(":productid", $productid);
 
-// select a particular user by id
-$stmt = $connection->prepare("DELETE FROM Producten WHERE id=:id"); 
-$stmt->bindParam(":id", $id);
-$stmt->execute();
+if ($stmt->execute()) {
+  $_SESSION["message"] = "Product deleted successfully";
+} else {
+  $_SESSION["message"] = "Error deleting product";
+}
 
 header("Location: dashboard.php");
+exit;
 ?>
-
